3b03810af169ff4b1999e369dde15de97a55f918
sanynote <sunnyjuice94@gmail.com>
2023-10-17 23:03:39 +0900

    이어 붙인 수

---
+++ programmers/Lv.0/이어 붙인 수.js
@@ -0,0 +1,28 @@
+// 정수가 담긴 리스트 num_list가 주어집니다. 
+// num_list의 홀수만 순서대로 이어 붙인 수와 짝수만 순서대로 이어 붙인 수의 합을 
+// return하도록 solution 함수를 완성해주세요.
+
+
+const arr = [1,4,3,9]
+
+function solution () {
+
+    let even = []
+    let odd = []
+
+    arr.forEach((item,index)=>{
+        if(item%2 === 0) {
+ even.push(String(item))
+
+        } else {
+            odd.push(String(item))
+        }
+    })
+
+   const answer= Number(even.join(''))+Number(odd.join(''))
+
+   return answer
+   
+}
+
+console.log(solution())
