ca1ffcfca4f9a54abe93e94734dcec770d59c372
sanynote <sunnyjuice94@gmail.com>
2024-01-22 20:47:05 +0900

    홀짝 구분하기

--- programmers/Lv.0/홀짝 구분하기.js
+++ programmers/Lv.0/홀짝 구분하기.js
@@ -1,13 +1,20 @@
 // 자연수 n이 입력으로 주어졌을 때 만약 n이 짝수이면 "n is even"을, 
 // 홀수이면 "n is odd"를 출력하는 코드를 작성해 보세요.
 
-function solution (n) {
-    if(n%2 === 0) {
-        console.log(n, 'is even')
-    } else {
-        console.log(n, 'is odd')
-    }
+// function solution (n) {
+//     if(n%2 === 0) {
+//         console.log(n, 'is even')
+//     } else {
+//         console.log(n, 'is odd')
+//     }
     
+// }
+
+// solution(1)
+
+function solution (a) {
+if(a % 2 === 0) console.log(a ,'is even')
+if(a % 2 === 1) console.log(a ,'is odd')
 }
 
-solution(1)
+solution(21)
