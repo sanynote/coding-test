585aa30f7dd242604ad1b402deec2f319c5a46b7
sanynote <sunnyjuice94@gmail.com>
2024-01-23 23:00:57 +0900

    자연수 뒤집어 배열로 만들기

--- programmers/Lv.1/자연수 뒤집어 배열로 만들기.js
+++ programmers/Lv.1/자연수 뒤집어 배열로 만들기.js
@@ -1,5 +1,9 @@
 // 자연수 뒤집어 배열로 만들기
 
+// function solution(n) {
+//     return n.toString().split("").reverse().map(Number)
+// }
+
 function solution(n) {
-    return n.toString().split("").reverse().map(Number)
+    return String(n).split('').reverse()
 }
